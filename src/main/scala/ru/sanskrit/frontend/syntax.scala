d5e6307f028f3aea0f075693491757ade0db7828
Jijasan <y.ike@mail.ru>
2024-02-01 14:11:16 +0300

    Add getType method

--- src/main/scala/ru/sanskrit/frontend/syntax.scala
+++ src/main/scala/ru/sanskrit/frontend/syntax.scala
@@ -1,13 +1,21 @@
 package ru.sanskrit.frontend
 
+import cats.Applicative
+import cats.syntax.applicative.*
 import ru.sanskrit.common.Type
 
 object syntax:
-
   enum Expr[F[_]]:
     case Lit(x: Int)
     case Var(name: String, `type`: F[Type])
     case App(f: Expr[F], x: Expr[F],`type`: F[Type])
     case Lam(arg: Var[F], expr: Expr[F], `type`: F[Type])
+
+    def getType(using Applicative[F]): F[Type] = this match {
+      case Lit(_)       => Type.Int.pure
+      case Var(_, t)    => t
+      case App(_, _, t) => t
+      case Lam(_, _, t) => t
+    }
 
   case class Func(name: String, tp: Option[Type], body: Expr[Option], args: Expr.Var[Option]*)
