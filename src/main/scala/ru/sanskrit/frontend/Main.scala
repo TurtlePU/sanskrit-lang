60117df27a6adf87d743fb8784998740a5930878
Jijasan <y.ike@mail.ru>
2024-02-16 16:47:49 +0300

    Add main function

---
+++ src/main/scala/ru/sanskrit/frontend/Main.scala
@@ -0,0 +1,21 @@
+package ru.sanskrit.frontend
+
+import cats.syntax.traverse.*
+
+object Main {
+  private def readFile(fileName: String): String = {
+    val source = scala.io.Source.fromFile("file.txt")
+    val res    = source.getLines.mkString("\n")
+    source.close()
+    res
+  }
+
+  def main(args: Array[String]): Unit = {
+    val files = args.map(readFile).toList
+    for {
+      parsed      <- files.traverse(file => parser.funcParser.rep0.parse(file).toOption.map(_._2))
+      typechecked <- parsed.traverse(file => file.traverse(func => typecheck.inferFuncType(func)))
+      result      <- typechecked.traverse(file => desugar.desugarProgram(file))
+    } yield println(result)
+  }
+}
