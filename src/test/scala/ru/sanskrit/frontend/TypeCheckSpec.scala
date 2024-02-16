2ea20d04dd6cdc98ccd475635bea74a4301e5d70
Jijasan <y.ike@mail.ru>
2024-02-16 16:11:13 +0300

    Add func parsing and typechecking

--- src/test/scala/ru/sanskrit/frontend/TypeCheckSpec.scala
+++ src/test/scala/ru/sanskrit/frontend/TypeCheckSpec.scala
@@ -4,25 +4,41 @@ import cats.Id
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 import ru.sanskrit.common.Type
-import ru.sanskrit.frontend.syntax.Expr
+import ru.sanskrit.frontend.syntax.{Expr, Func => FFunc}
+import ru.sanskrit.frontend.typecheck.{Func => TFunc}
 
 class TypeCheckSpec extends AnyFlatSpec with Matchers:
   "checkType" should "check literals to int" in {
     typecheck.inferType(Expr.Lit(42), Map.empty) shouldBe Some(Expr.Lit(42))
   }
 
-  it should "check variable in context" in {
+  it should "check a variable in the context" in {
     typecheck.inferType(Expr.Var("x", Some(Type.Int)), Map("x" -> Type.Int)) shouldBe Some(Expr.Var[Id]("x", Type.Int))
   }
 
-  it should "check variable not in context" in {
+  it should "check a variable not in the context" in {
     typecheck.inferType(Expr.Var("x", Some(Type.Int)), Map.empty) shouldBe Some(Expr.Var[Id]("x", Type.Int))
   }
 
-  "inferType" should "infer variable type from context" in {
+  "inferType" should "infer a variable type from the context" in {
     typecheck.inferType(Expr.Var("x", None), Map("x" -> Type.Int)) shouldBe Some(Expr.Var[Id]("x", Type.Int))
   }
 
-  it should "fail on variable not in context" in {
+  it should "fail on a variable not in the context" in {
     typecheck.inferType(Expr.Var("x", None), Map.empty) shouldBe None
+  }
+
+  "inferFuncType" should "accept a fully annotated function" in {
+    typecheck.inferFuncType(FFunc("id", Some(Type.Int), Expr.Var("x", None), Expr.Var("x", Some(Type.Int)))) shouldBe
+      Some(TFunc("id", Type.Int, List(Expr.Var("x", Type.Int)), Expr.Var("x", Type.Int)))
+  }
+
+  it should "infer a function type based on the argument types" in {
+    typecheck.inferFuncType(FFunc("id", None, Expr.Var("x", None), Expr.Var("x", Some(Type.Int)))) shouldBe
+      Some(TFunc("id", Type.Int, List(Expr.Var("x", Type.Int)), Expr.Var("x", Type.Int)))
+  }
+
+  it should "infer a argument type based on the function types" in {
+    typecheck.inferFuncType(FFunc("id", Some(Type.Int), Expr.Var("x", None), Expr.Var("x", None))) shouldBe
+      Some(TFunc("id", Type.Int, List(Expr.Var("x", Type.Int)), Expr.Var("x", Type.Int)))
   }
