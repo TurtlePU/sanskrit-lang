e11a9cab0ad0b1cb0b628931cc1134fc31cb33f1
Jijasan <y.ike@mail.ru>
2024-02-29 19:11:58 +0300

    Update Expr model

--- src/main/scala/ru/sanskrit/common/Expr.scala
+++ src/main/scala/ru/sanskrit/common/Expr.scala
@@ -2,18 +2,14 @@ package ru.sanskrit.common
 
 case class Name(name: String)
 
-sealed trait Rhs
-object Rhs:
-  case class Val(v: Expr.Val) extends Rhs
-  case class App(f: Expr.Val, x: Expr.Val) extends Rhs
-  case class Sum(a: Expr.Val, b: Expr.Val) extends Rhs
-  case class Mul(a: Expr.Val, b: Expr.Val) extends Rhs
-  case class Abs(x: Name, t: Expr) extends Rhs
-
 sealed trait Expr
-object Expr:
-  case class Let(x: Name, t: Type, v: Rhs, b: Expr) extends Expr
-  sealed trait Val extends Expr
-  object Val:
-    case class Lit(x: Int) extends Val
-    case class Var(x: Name) extends Val
+case class Let(x: Name, t: Type, v: Expr, b: Expr) extends Expr
+
+sealed trait Val extends Expr
+case class Lit(x: Int) extends Val
+case class Var(x: Name) extends Val
+case class Abs(x: Name, t: Expr) extends Val
+
+case class App(f: Var, x: Var) extends Expr
+case class Sum(a: Var, b: Var) extends Expr
+case class Mul(a: Var, b: Var) extends Expr
