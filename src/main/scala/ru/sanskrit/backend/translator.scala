fd69c6cd90fb72ff661a1652c9de409071583d47
Anton Andruschenko <jacques.sudaques@yandex.ru>
2024-05-04 23:09:36 +0300

    fix

--- src/main/scala/ru/sanskrit/backend/translator.scala
+++ src/main/scala/ru/sanskrit/backend/translator.scala
@@ -5,24 +5,27 @@ import scala.collection.mutable.Map
 
 object translator {
     type Closures = Map[Type, String]
-    type TypeEnv = Map[Name, Type]
+    type Env = Map[Name, (Type, String)]
     type Definitions = Map[String, String]
 
     def run(e: Expr): Option[String] =
         val closures: Closures = Map.empty
         val definitions: Definitions = Map.empty
-        val body = exprToC(e, closures, Map.empty, definitions)
+        val env: Env = Map.empty
+        val body = exprToC(e, closures, env, definitions)
 
-        val closuresCode = closures.values.mkString
-        val definitionsCode = definitions.values.mkString
+        val closuresCode = closures.values.mkString("\n")
+        val definitionsCode = definitions.values.mkString("\n")
+        val globalScope = List(closuresCode, definitionsCode).filter(x => !x.isEmpty).mkString("\n\n")
 
-        Some(s"""
-        |#include <stdio.h>
+        Some(s"""#include <stdio.h>
+        |#include <stdlib.h>
         |
-        |$closuresCode
-        |$definitionsCode
+        |$globalScope
+        |
         |int main() {
         |$body
+        |${freeMemory(env)}
         |return 0;
         |}
         |""".stripMargin)
@@ -36,27 +39,38 @@ object translator {
             val argType = typeToC(a, closures)
             val returnType = typeToC(b, closures)
             val closureName = s"${argType}_${returnType}_closure"
-            val declaration = s"""
-            |typedef struct {
+            val declaration = s"""typedef struct {
             |   $returnType (*impl)($argType, void*);
             |   void* env;
-            |} $closureName;
-            |""".stripMargin
+            |} $closureName;""".stripMargin
             closures += (Type.Func(a, b) -> declaration)
             closureName
     }
 
-    private def exprToC(e: Expr, closures: Closures, env: TypeEnv, definitions: Definitions, absType: Option[Type] = None): String = e match {
+    private def exprToC(
+        e: Expr,
+        closures: Closures,
+        env: Env,
+        definitions: Definitions,
+        absType: Option[Type] = None,
+        cEnv: String = ""
+    ): String = e match {
         case Lit(x) => x.toString
-        case Var(x) => cleanName(x)
-        case Sum(a, b) => s"${exprToC(a, closures, env, definitions)} + ${exprToC(b, closures, env, definitions)}"
-        case Mul(a, b) => s"${exprToC(a, closures, env, definitions)} * ${exprToC(b, closures, env, definitions)}"
+        case Var(x) => cEnv + cleanName(x)
+        case Sum(a, b) =>
+            val aCode = exprToC(a, closures, env, definitions, absType, cEnv)
+            val bCode = exprToC(b, closures, env, definitions, absType, cEnv)
+            s"$aCode + $bCode"
+        case Mul(a, b) =>
+            val aCode = exprToC(a, closures, env, definitions, absType, cEnv)
+            val bCode = exprToC(b, closures, env, definitions, absType, cEnv)
+            s"$aCode * $bCode"
         case Let(x, t, v, b) =>
-            val value = exprToC(v, closures, env, definitions, Some(t))
-            env += (x -> t)
+            val value = exprToC(v, closures, env, definitions, absType=Some(t))
+            env += (x -> (t, value))
             val bodyCode = b match {
-                case v: Var => s"printf(\"%d\\n\", ${exprToC(b, closures, env, definitions, Some(t))});"
-                case _ => exprToC(b, closures, env, definitions, Some(t))
+                case v: Var => s"printf(\"%d\\n\", ${exprToC(b, closures, env, definitions, absType=Some(t))});"
+                case _ => exprToC(b, closures, env, definitions, absType=Some(t))
             }
             s"""${typeToC(t, closures)} ${cleanName(x)} = $value;
             |$bodyCode""".stripMargin
@@ -65,16 +79,41 @@ object translator {
           val closureType = typeToC(funcType, closures)
           val argType = typeToC(funcType.a, closures)
           val returnType = typeToC(funcType.b, closures)
-          val bodyCode = exprToC(body, closures, env + (x -> funcType.a), definitions)
+          val bodyCode = exprToC(body, closures, env, definitions, cEnv="env->")
           val implName = s"func_${bodyCode.hashCode.abs}"
-          definitions += (implName -> s"""
-          |$returnType $implName($argType ${x.name}, void* env) {
+          val envName = s"env_${bodyCode.hashCode.abs}"
+          definitions += (implName -> s"""${envStructToC(env + (x -> (funcType.a, "")), envName, closures)}
+          |
+          |$returnType $implName($argType ${x.name}, void* data) {
+          |$envName* env = ($envName*)data;
+          |env->${x.name} = ${x.name};
           |return $bodyCode;
           |}
-          |""".stripMargin)
-          s"($closureType){.impl=$implName, .env=NULL}"
+          |
+          |$closureType create_$implName() {
+          |$envName* env = malloc(sizeof($envName));
+          |${populateCEnv(env)}
+          |return ($closureType){.impl=$implName, .env=env};
+          |}""".stripMargin)
+          s"create_$implName()"
         case App(f, x) =>
-          val closureType = typeToC(env(f.x), closures)
+          val closureType = typeToC(env(f.x)._1, closures)
           s"(${cleanName(f.x)}.impl(${cleanName(x.x)}, ${cleanName(f.x)}.env))"
     }
+
+    private def envStructToC(env: Env, envName: String, closures: Closures): String =
+        val variables = (for {(name, (t, _)) <- env} yield s"${typeToC(t, closures)} ${cleanName(name)};").mkString("\n")
+        s"""typedef struct {
+        |$variables
+        |} $envName;""".stripMargin
+
+    private def populateCEnv(env: Env): String =
+        (for {(name, (_, v)) <- env} yield s"env->${cleanName(name)} = $v;").mkString("\n")
+
+    private def freeMemory(env: Env): String =
+        val closuresEnv = env.filter(x => x._2._1 match {
+            case Type.Func(_, _) => true
+            case _ => false
+        })
+        (for {(name, (t, _)) <- closuresEnv} yield s"free(${cleanName(name)}.env);").mkString("\n")
 }
--- src/test/scala/ru/sanskrit/backend/TranslatorSpec.scala
+++ src/test/scala/ru/sanskrit/backend/TranslatorSpec.scala
@@ -8,14 +8,15 @@ import org.scalatest.matchers.should.Matchers
 class TranslatorSpec extends AnyFlatSpec with Matchers {
   "Translator" should "translate literal main as printf" in {
     val expr = Let(Name("main"), Type.Int, Lit(5), Var(Name("main")))
-    val expectedTranslation = """
-    |#include <stdio.h>
+    val expectedTranslation = """#include <stdio.h>
+    |#include <stdlib.h>
     |
     |
     |
     |int main() {
     |int main = 5;
     |printf("%d\n", main);
+    |
     |return 0;
     |}
     |""".stripMargin
@@ -40,25 +41,36 @@ class TranslatorSpec extends AnyFlatSpec with Matchers {
       )
     )
 
-    val expectedTranslation = """
-    |#include <stdio.h>
-    |
+    val expectedTranslation = """#include <stdio.h>
+    |#include <stdlib.h>
     |
     |typedef struct {
     |   int (*impl)(int, void*);
     |   void* env;
     |} int_int_closure;
     |
+    |typedef struct {
+    |int x;
+    |} env_1076984342;
     |
-    |int func_111817306(int x, void* env) {
-    |return x * x;
+    |int func_1076984342(int x, void* data) {
+    |env_1076984342* env = (env_1076984342*)data;
+    |env->x = x;
+    |return env->x * env->x;
+    |}
+    |
+    |int_int_closure create_func_1076984342() {
+    |env_1076984342* env = malloc(sizeof(env_1076984342));
+    |
+    |return (int_int_closure){.impl=func_1076984342, .env=env};
     |}
     |
     |int main() {
-    |int_int_closure f = (int_int_closure){.impl=func_111817306, .env=NULL};
+    |int_int_closure f = create_func_1076984342();
     |int x = 5;
     |int main = (f.impl(x, f.env));
     |printf("%d\n", main);
+    |free(f.env);
     |return 0;
     |}
     |""".stripMargin
