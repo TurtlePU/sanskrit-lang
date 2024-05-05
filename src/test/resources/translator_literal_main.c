4053a74fd5aa96750d547dac15d245463aca319c
Anton Andruschenko <jacques.sudaques@yandex.ru>
2024-05-05 15:35:18 +0300

    move c from tests

---
+++ src/test/resources/translator_literal_main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main() {
+int main = 5;
+printf("%d\n", main);
+
+return 0;
+}
--- src/test/scala/ru/sanskrit/backend/TranslatorSpec.scala
+++ src/test/scala/ru/sanskrit/backend/TranslatorSpec.scala
@@ -8,18 +8,11 @@ import org.scalatest.matchers.should.Matchers
 class TranslatorSpec extends AnyFlatSpec with Matchers {
   "Translator" should "translate literal main as printf" in {
     val expr = Let(Name("main"), Type.Int, Lit(5), Var(Name("main")))
-    val expectedTranslation = """#include <stdio.h>
-    |#include <stdlib.h>
-    |
-    |
-    |
-    |int main() {
-    |int main = 5;
-    |printf("%d\n", main);
-    |
-    |return 0;
-    |}
-    |""".stripMargin
+
+    val translationFile = scala.io.Source.fromResource("translator_literal_main.c")
+    val expectedTranslation = translationFile.mkString
+    translationFile.close()
+
     translator.run(expr) shouldBe Some(expectedTranslation)
   }
 
@@ -41,39 +34,9 @@ class TranslatorSpec extends AnyFlatSpec with Matchers {
       )
     )
 
-    val expectedTranslation = """#include <stdio.h>
-    |#include <stdlib.h>
-    |
-    |typedef struct {
-    |   int (*impl)(int, void*);
-    |   void* env;
-    |} int_int_closure;
-    |
-    |typedef struct {
-    |int x;
-    |} env_1076984342;
-    |
-    |int func_1076984342(int x, void* data) {
-    |env_1076984342* env = (env_1076984342*)data;
-    |env->x = x;
-    |return env->x * env->x;
-    |}
-    |
-    |int_int_closure create_func_1076984342() {
-    |env_1076984342* env = malloc(sizeof(env_1076984342));
-    |
-    |return (int_int_closure){.impl=func_1076984342, .env=env};
-    |}
-    |
-    |int main() {
-    |int_int_closure f = create_func_1076984342();
-    |int x = 5;
-    |int main = (f.impl(x, f.env));
-    |printf("%d\n", main);
-    |free(f.env);
-    |return 0;
-    |}
-    |""".stripMargin
+    val translationFile = scala.io.Source.fromResource("translator_function_call.c")
+    val expectedTranslation = translationFile.mkString
+    translationFile.close()
 
     translator.run(expr) shouldBe Some(expectedTranslation)
   }
