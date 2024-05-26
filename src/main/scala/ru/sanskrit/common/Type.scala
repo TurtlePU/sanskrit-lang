7d369c68215ecf6806a988dc52e5dc9750f5b80d
Jijasan <y.ike@mail.ru>
2024-05-26 15:40:54 +0300

    Fix Sigma

--- src/main/scala/ru/sanskrit/common/Type.scala
+++ src/main/scala/ru/sanskrit/common/Type.scala
@@ -3,4 +3,4 @@ package ru.sanskrit.common
 enum Type:
   case Int
   case Func(a: Type, b: Type)
-  case Sigma(aVal: Expr, aType: Type, bType: Type)
+  case Sigma(aVal: Var, aType: Type, bType: Type)
